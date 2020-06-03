0c32c9424a03ddef9ad4d78311d1b80367e43fb1
BioXtrem <37130499+BioXtrem@users.noreply.github.com>
2020-06-03 09:31:09 -0500

    Route

--- src/components/InicioRegistro.vue
+++ src/components/InicioRegistro.vue
@@ -75,6 +75,7 @@
 
 <script>
   export default {
+    name:'',
     data() {
       return {
         form:{
--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+import InicioRegistro from './components/InicioRegistro'
+import Pokemon from './components/Pokemon'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [{
+    path: '/',
+    name: 'sesion',
+    component: InicioRegistro,
+  }, {
+    path: '/home',
+    component: Pokemon,
+  }]
+})
+
+/* router.beforeEach((to, from, next) => {
+  if (to.path == '/' && !isAuthenticated) next({ name: 'Login' })
+  else next()
+}) */
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
